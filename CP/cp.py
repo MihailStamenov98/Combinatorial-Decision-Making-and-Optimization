a009113bb28217160b9988f50aff95ee0f14ffca
Mihail Stamenov <mpstamenov@gmail.com>
2022-09-17 15:52:46 +0300

    import data to model

--- CP/cp.py
+++ CP/cp.py
@@ -1,28 +1,29 @@
 from minizinc import Instance, Model, Solver
 import os
 
+chip_width = "chip_width"
+n_blocks = "n_blocks"
+dimensions = "dimensions"
+chip_height = "chip_height"
 
-for i in range(1, 40):
+for i in range(1, 2):
     cur_path = os.path.dirname(__file__)
-    print(cur_path)
-    new_path = os.path.relpath(f'instances\\ins-{i}.txt', cur_path)
-    print(new_path)
-    with open(new_path, 'r') as f:
-        w = [int(x) for x in next(f).split()]
-        h = [int(x) for x in next(f).split()]
+    rel_path = f'../instances/ins-{i}.txt'
+    with open(cur_path+'\\'+rel_path, 'r') as f:
+        w = [int(x) for x in next(f).split()][0]
+        n = [int(x) for x in next(f).split()][0]
         array = []
         for line in f: # read rest of lines
             array.append([int(x) for x in line.split()])
+        model = Model(cur_path+"\\VLSI.mzn")
+        gecode = Solver.lookup("gecode")
+        instance = Instance(gecode, model)
+        instance[chip_width] = w
+        instance[n_blocks] = n
+        instance[dimensions] = array
+        result = instance.solve()
+        #print(result["blocks_x"])
+        #print(result["blocks_y"])
+        #print(result[chip_width])
 
 
-## Load n-Queens model from file
-#nqueens = Model("./nqueens.mzn")
-## Find the MiniZinc solver configuration for Gecode
-#gecode = Solver.lookup("gecode")
-## Create an Instance of the n-Queens model for Gecode
-#instance = Instance(gecode, nqueens)
-## Assign 4 to n
-#instance["n"] = 4
-#result = instance.solve()
-## Output the array q
-#print(result["q"])
