2d24da2d7e86dbc89b50806cc6cfe37bb77b4dd6
Mihail Stamenov <mpstamenov@gmail.com>
2022-09-18 18:21:33 +0300

    return data from solver

--- CP/cp.py
+++ CP/cp.py
@@ -1,29 +1,42 @@
 from minizinc import Instance, Model, Solver
+from typing import List
+from dataclasses import InitVar, dataclass
 import os
+import sys
+cur_path = os.path.dirname(__file__)
+PROJECT_ROOT = os.path.abspath(os.path.join(
+                  cur_path, 
+                  os.pardir)
+)
+sys.path.append(PROJECT_ROOT)
 
-chip_width = "chip_width"
-n_blocks = "n_blocks"
+from utils import readFile
+
+
+@dataclass
+class BlocksDistribution:
+    def __init__(self, blocks_x, blocks_y, objective):
+        self.blocks_x = blocks_x
+        self.blocks_x = blocks_y
+        self.chipHeight = objective
+    __output_item: InitVar[str]
+
+
+chipWidth = "chip_width"
+nBlocks = "n_blocks"
 dimensions = "dimensions"
-chip_height = "chip_height"
+chipHeight = "chip_height"
+
 
 for i in range(1, 2):
-    cur_path = os.path.dirname(__file__)
-    rel_path = f'../instances/ins-{i}.txt'
-    with open(cur_path+'\\'+rel_path, 'r') as f:
-        w = [int(x) for x in next(f).split()][0]
-        n = [int(x) for x in next(f).split()][0]
-        array = []
-        for line in f: # read rest of lines
-            array.append([int(x) for x in line.split()])
-        model = Model(cur_path+"\\VLSI.mzn")
-        gecode = Solver.lookup("gecode")
-        instance = Instance(gecode, model)
-        instance[chip_width] = w
-        instance[n_blocks] = n
-        instance[dimensions] = array
-        result = instance.solve()
-        #print(result["blocks_x"])
-        #print(result["blocks_y"])
-        #print(result[chip_width])
-
-
+    data = readFile(i)
+    model = Model(cur_path+"\\VLSI.mzn")
+    model.output_type = BlocksDistribution
+    gecode = Solver.lookup("gecode")
+    instance = Instance(gecode, model)
+    instance[chipWidth] = data.w
+    instance[nBlocks] = data.n
+    instance[dimensions] = data.dimensions
+    result = instance.solve().solution
+    assert type(result) == BlocksDistribution
+    print(result.blocks_x)
